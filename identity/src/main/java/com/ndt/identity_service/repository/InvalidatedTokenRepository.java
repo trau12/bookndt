a44e14538c7771897917e054578a9eb04181e028
trau12 <tairazerx@gmail.com>
2024-08-01 19:57:34 +0700

    Remove identity directory

--- identity/src/main/java/com/ndt/identity_service/repository/InvalidatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ndt.identity_service.repository;
-
-import com.ndt.identity_service.entity.InvalidatedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
-}
